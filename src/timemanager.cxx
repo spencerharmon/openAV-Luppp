039cbd335173b2acd4fa9b52666a278e1c84f538
Spencer Harmon <spencerharmon.91@gmail.com>
2018-01-23 00:37:04 -0600

    catch up

--- src/timemanager.cxx
+++ src/timemanager.cxx
@@ -40,22 +40,25 @@ TimeManager::TimeManager():
 	// 120 BPM default
 	if(jack->_timebase_master){
 		fpb = samplerate / 2;
+		//Counter for current bar/beat
+		barCounter  = 0;
+		beatCounter = 0;
+
+		previousBeat = 0;
+
+		//In process() we want to immediately process bar(), beat() of all observers
+		// thats why beatFrameCountdown<nframes, but we don't know yet what value nframes has
+		// so set beatFrameCountdown to a value that garantees beatFrameCountdown<nframes
+		beatFrameCountdown = -1;//fpb;
+
+		totalFrameCounter = 0;
 	}else{
-		
+//		client = jack->getJackClientPointer();
+//		jack_position_t tpos;
+//		jack_transport_state_t tstate = jack_transport_query( client, &tpos);
+//		fpb = samplerate * 60 / tpos.beats_per_minute;
+		fpb = 1;
 	}
-
-	//Counter for current bar/beat
-	barCounter  = 0;
-	beatCounter = 0;
-
-	previousBeat = 0;
-
-	//In process() we want to immediately process bar(), beat() of all observers
-	// thats why beatFrameCountdown<nframes, but we don't know yet what value nframes has
-	// so set beatFrameCountdown to a value that garantees beatFrameCountdown<nframes
-	beatFrameCountdown = -1;//fpb;
-
-	totalFrameCounter = 0;
 
 	tapTempoPos = 0;
 	tapTempo[0] = 0;
@@ -171,33 +174,25 @@ void TimeManager::setTransportState( TRANSPORT_STATE s )
 		jack->transportRolling(false);
 	else {
 		jack->transportRolling(true);
-		barCounter  = 0;
-		beatCounter = 0;
-		beatFrameCountdown = -1;
-		for(int i=0; i<observers.size(); i++)
-			observers[i]->resetTimeState();
+		if(jack->_timebase_master){
+			barCounter  = 0;
+			beatCounter = 0;
+			beatFrameCountdown = -1;
+			for(unsigned int i=0; i<observers.size(); i++)
+				observers[i]->resetTimeState();
+		}
 	}
 }
 
-void TimeManager::process(Buffers* buffers)
+void TimeManager::masterProcess(Buffers* buffers)
 {
-	if(!jack->_timebase_master){
-		jack_transport_state_t tstate;
-		jack_position_t tpos;
-		tstate = jack_transport_query(jack->getJackClientPointer(), &tpos);
-		if(tstate == JackTransportStopped){
-			setTransportState(TRANSPORT_STOPPED);
-		}
-	}
 	// time signature?
 	//buffers->transportPosition->beats_per_bar = 4;
 	//buffers->transportPosition->beat_type     = 4;
 	if ( transportState == TRANSPORT_STOPPED ) {
 		return;
 	}
-
 	int nframes = buffers->nframes;
-
 	if ( beatFrameCountdown < nframes ) {
 		//length of beat is not multiple of nframes, so need to process last frames of last beat *before* setting next beat
 		//then set new beat (get the queued actions: play, rec etc)
@@ -228,32 +223,15 @@ void TimeManager::process(Buffers* buffers)
 			observers.at(i)->beat();
 		}
 
-		if(jack->_timebase_master){
-			if(buffers->transportPosition){
-				if ( beatCounter % (int)buffers->transportPosition->beats_per_bar == 0 ) {
-					// inform observers of new bar SECOND
-					for(uint i = 0; i < observers.size(); i++) {
-						observers.at(i)->bar();
-					}
-					barCounter++;
-					//beatCounter=0;
+		if(buffers->transportPosition){
+			if ( beatCounter % (int)buffers->transportPosition->beats_per_bar == 0 ) {
+				// inform observers of new bar SECOND
+				for(uint i = 0; i < observers.size(); i++) {
+					observers.at(i)->bar();
 				}
+				barCounter++;
+				//beatCounter=0;
 			}
-		}else{
-			jack_position_t tpos;
-			jack_transport_query( jack->getJackClientPointer(), &tpos);
-
-//			if(tpos.valid & JackPositionBBT){
-				if ( tpos.beat == 1 ) {
-					// inform observers of new bar SECOND
-					for(uint i = 0; i < observers.size(); i++) {
-						observers.at(i)->bar();
-					}
-					barCounter++;
-					//beatCounter=0;
-				}
-				setBpm(tpos.beats_per_minute);
-//			}
 		}
 
 		// process after
@@ -267,57 +245,136 @@ void TimeManager::process(Buffers* buffers)
 		writeToGuiRingbuffer( &e );
 
 
-
 		beatFrameCountdown = fpb-after;
 		beatCounter++;
 	} else {
 		jack->processFrames( nframes );
 		beatFrameCountdown -= nframes;
-
 	}
-
 	totalFrameCounter += nframes;
-	if(jack->_timebase_master){
-		// write BPM / transport info to JACK
+	// write BPM / transport info to JACK
 
-		int bpm = ( samplerate * 60) / fpb;
-		if ( buffers->transportPosition ) {
-				buffers->transportPosition->valid = (jack_position_bits_t)(JackPositionBBT | JackTransportPosition);
-
-				buffers->transportPosition->bar  = beatCounter / 4 + 1;// bars 1-based
-				buffers->transportPosition->beat = (beatCounter % 4) + 1; // beats 1-4
-
-				float part = float( fpb-beatFrameCountdown) / fpb;
-				buffers->transportPosition->tick = part > 1.0f? 0.9999*1920 : part*1920;
-
-				buffers->transportPosition->frame = totalFrameCounter;
-
-				buffers->transportPosition->ticks_per_beat = 1920;
-				buffers->transportPosition->beats_per_bar = 4;
-
-				buffers->transportPosition->beats_per_minute = bpm;
-		}
-	}/*else{
-		jack_position_t tpos;
-		jack_transport_state_t tstate;
-		tstate = jack_transport_query( jack->getJackClientPointer(), &tpos);
-		if(tpos.valid & JackPositionBBT){
+	int bpm = ( samplerate * 60) / fpb;
+	if ( buffers->transportPosition ) {
 			buffers->transportPosition->valid = (jack_position_bits_t)(JackPositionBBT | JackTransportPosition);
 
-			buffers->transportPosition->bar  = tpos.beat;
-			buffers->transportPosition->beat = tpos.beat;
+			buffers->transportPosition->bar  = beatCounter / 4 + 1;// bars 1-based
+			buffers->transportPosition->beat = (beatCounter % 4) + 1; // beats 1-4
 
-			buffers->transportPosition->tick = tpos.tick;
+			float part = float( fpb-beatFrameCountdown) / fpb;
+			buffers->transportPosition->tick = part > 1.0f? 0.9999*1920 : part*1920;
 
-			buffers->transportPosition->frame = tpos.frame;
+			buffers->transportPosition->frame = totalFrameCounter;
 
-			buffers->transportPosition->ticks_per_beat = tpos.ticks_per_beat;
-			buffers->transportPosition->beats_per_bar = tpos.beats_per_bar;
-			setBpm(tpos.beats_per_minute);
+			buffers->transportPosition->ticks_per_beat = 1920;
+			buffers->transportPosition->beats_per_bar = 4;
 
-			buffers->transportPosition->beats_per_minute = tpos.beats_per_minute;
-		}
+			buffers->transportPosition->beats_per_minute = bpm;
 	}
-*/
+}
+void TimeManager::clientProcess(Buffers* buffers)
+{
+	jack_client_t * client = jack->getJackClientPointer();
+	int nframes = buffers->nframes;
+
+	jack_transport_state_t tstate;
+	jack_position_t tpos;
+	tstate = jack_transport_query( client, &tpos);
+
+	int bpm = samplerate * 60 / fpb;
+	if( bpm != tpos.beats_per_minute ){
+		//setBpm shouldn't be used by jack timebase client because it resets counters. use setFpb directly.
+//		setFpb( samplerate / tpos.beats_per_minute * 60 ); This is wrong, right?
+		setFpb( samplerate * 60 / tpos.beats_per_minute );
+	}
+
+	if(tstate == JackTransportStopped){
+		setTransportState(TRANSPORT_STOPPED);
+	}else if(tstate == JackTransportRolling){
+		setTransportState(TRANSPORT_ROLLING);
+	}
+	if ( transportState == TRANSPORT_STOPPED ) {
+		return;
+	}
+
+	//adjust to zero-based for frame maths
+	int thisbeat = ((int)tpos.bar - 1) * (int)tpos.beats_per_bar + ((int)tpos.beat - 1);
+	unsigned int nextBeatFrame = (thisbeat +1) * fpb;
+
+	jack_nframes_t startframe = tpos.frame;
+	jack_nframes_t endframe = startframe + nframes - 1;
+
+	char buffer[50];
+//	sprintf (buffer, "startframe: %i", (int) startframe);
+//	EventGuiPrint e0( buffer );
+//	writeToGuiRingbuffer( &e0 );
+
+//	sprintf (buffer, "endframe: %i", (int) endframe);
+//	EventGuiPrint e1( buffer );
+//	writeToGuiRingbuffer( &e1 );
+
+//	sprintf (buffer, "nextBeatFrame: %u", (unsigned int) nextBeatFrame);
+//	EventGuiPrint e2( buffer );
+//	writeToGuiRingbuffer( &e2 );
+
+
+	if ( nextBeatFrame >= startframe && nextBeatFrame <= endframe ) {
+		//length of beat is not multiple of nframes, so need to process last frames of last beat *before* setting next beat
+		//then set new beat (get the queued actions: play, rec etc)
+		// then process first frames *after* new beat
+
+		int before = nextBeatFrame - startframe;
+		int after = endframe - nextBeatFrame + 1;
+
+		if ( before < nframes && after <= nframes && before + after == nframes ) {
+			char buffer [50];
+		} else {
+			char buffer [50];
+			sprintf (buffer, "Timing Error: before: %i, after %i", before, after );
+			EventGuiPrint e2( buffer );
+			writeToGuiRingbuffer( &e2 );
+		}
+
+		// process before beat:
+		if(before)
+			jack->processFrames( before );
+
+		// handle beat:
+		// inform observers of new beat FIRST
+		for(uint i = 0; i < observers.size(); i++) {
+			observers.at(i)->beat();
+		}
+
+		//check if this cycle's new beat is also a new bar
+		//presumably master will have set the new beat to 1 if startframe is the nextbeatframe.
+		//otherwise, it will still be the last beat in the bar
+		if ( ( nextBeatFrame == startframe && tpos.beat ==1 ) || tpos.beat == tpos.beats_per_bar ) {
+			// inform observers of new bar SECOND
+			for(uint i = 0; i < observers.size(); i++) {
+				observers.at(i)->bar();
+			}
+			barCounter++;
+		}
+
+		// process after
+		// we need to clear internal buffers in order to write *after* frames to them
+		jack->clearInternalBuffers(nframes);
+		if(after)
+			jack->processFrames( after );
+
+		// write new beat to UI (bar info currently not used)
+		EventTimeBarBeat e( tpos.bar, tpos.beat );
+		writeToGuiRingbuffer( &e );
+	} else {
+		jack->processFrames( nframes );
+	}
+}
+void TimeManager::process(Buffers* buffers)
+{
+	if(jack->_timebase_master){
+		masterProcess(buffers);
+	}else{
+		clientProcess(buffers);
+	}
 }
 
